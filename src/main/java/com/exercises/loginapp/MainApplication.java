c0ddb224f2e2c619617cb6166cf2baf4d3b8b49a
MrArcano <salvatore-dario@hotmail.it>
2024-06-04 17:48:14 +0200

    [add]: check username and password

--- src/main/java/com/exercises/loginapp/MainApplication.java
+++ src/main/java/com/exercises/loginapp/MainApplication.java
@@ -5,14 +5,17 @@ import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.kordamp.bootstrapfx.BootstrapFX;
 
 import java.io.IOException;
 
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("main-view.fxml"));
-        stage.setScene(new Scene(root));
+        Parent root = FXMLLoader.load(getClass().getResource("fxml/main-view.fxml"));
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("css/style.css").toExternalForm());
+        stage.setScene(scene);
         stage.setTitle("Floating Placeholder TextField");
         stage.show();
     }
--- src/main/java/com/exercises/loginapp/MainController.java
+++ src/main/java/com/exercises/loginapp/MainController.java
@@ -2,9 +2,12 @@ package com.exercises.loginapp;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 
 public class MainController {
@@ -14,6 +17,12 @@ public class MainController {
 
     @FXML
     private URL location;
+
+    @FXML
+    private Label errorPassword;
+
+    @FXML
+    private Label errorUsername;
 
     @FXML
     private TextField inputPassword;
@@ -27,6 +36,57 @@ public class MainController {
     @FXML
     private Label lblUsername;
 
+    private final int MIN_LENGTH_PASSWORD = 7;
+
+    @FXML
+    void doCheckPassword(KeyEvent event) {
+        String inputText = inputPassword.getText().trim();
+
+        if(inputText.length() < MIN_LENGTH_PASSWORD || !hasNumber(inputText) || !hasMixedCase(inputText) || !hasSpecialChar(inputText)){
+            errorPassword.setText("Password non valida!");
+        }else {
+            errorPassword.setText("");
+        }
+    }
+
+    // Verifica se la stringa contiene caratteri speciali [!,?,@,#]
+    private static boolean hasSpecialChar(String inputText) {
+        boolean hasSpecialChar = inputText.matches(".*[!?@#].*");
+        // System.out.println("String: " + (hasSpecialChar ? "hasSpecialChar" : "noSpecialChar")); //debug
+        return hasSpecialChar;
+    }
+
+    // Verifica se la stringa ha numeri
+    private static boolean hasNumber(String inputText) {
+        boolean hasNumber = inputText.matches(".*[0-9].*");
+        // System.out.println("String: " + (hasNumber ? "hasNumber" : "noNumber")); //debug
+        return hasNumber;
+    }
+
+    // Verifica se la stringa ha sia caratteri maiuscoli che minuscoli
+    private static boolean hasMixedCase(String inputText) {
+        String lowerString = inputText.toLowerCase();
+        String upperString = inputText.toUpperCase();
+        boolean hasMixedCase = !inputText.equals(lowerString) && !inputText.equals(upperString);
+        // System.out.println("String: " + (hasMixedCase ? "MultiCase" : "SingleCase")); //debug
+        return hasMixedCase;
+    }
+
+    @FXML
+    void doCheckUsername(KeyEvent event) {
+        String inputText = inputUsername.getText().trim();
+        if (inputText.isEmpty()) {
+            errorUsername.setText("Username non valido!");
+        } else {
+            errorUsername.setText("");
+        }
+    }
+
+    @FXML
+    public void doLogin(ActionEvent actionEvent) {
+        
+    }
+
     @FXML
     void onClickLblUsername(MouseEvent event) {
         inputUsername.requestFocus();
@@ -37,15 +97,20 @@ public class MainController {
         inputPassword.requestFocus();
     }
 
-
-
     @FXML
     void initialize() {
+
+        errorUsername.setText("");
+        errorPassword.setText("");
+
+        assert errorPassword != null : "fx:id=\"errorPassword\" was not injected: check your FXML file 'main-view.fxml'.";
+        assert errorUsername != null : "fx:id=\"errorUsername\" was not injected: check your FXML file 'main-view.fxml'.";
         assert inputPassword != null : "fx:id=\"inputPassword\" was not injected: check your FXML file 'main-view.fxml'.";
         assert inputUsername != null : "fx:id=\"inputUsername\" was not injected: check your FXML file 'main-view.fxml'.";
         assert lblPassword != null : "fx:id=\"lblPassword\" was not injected: check your FXML file 'main-view.fxml'.";
         assert lblUsername != null : "fx:id=\"lblUsername\" was not injected: check your FXML file 'main-view.fxml'.";
 
     }
+
 
 }
